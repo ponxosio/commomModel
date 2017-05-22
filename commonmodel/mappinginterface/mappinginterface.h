209111e25b73b234f76773f30aa3f2d1d850be06
ponxosio <ponxosio@gmail.com>
2017-05-22 10:10:41 +0200

    change mapping interface to be more general.

--- commonmodel/mappinginterface/mappinginterface.h
+++ commonmodel/mappinginterface/mappinginterface.h
@@ -1,7 +1,9 @@
 #ifndef MAPPINGINTERFACE_H
 #define MAPPINGINTERFACE_H
 
+#include <cstdarg>
 #include <memory>
+#include <stdexcept>
 #include <string>
 
 #include <protocolGraph/ProtocolGraph.h>
@@ -10,7 +12,7 @@ class MappingInterface {
 public:
     virtual ~MappingInterface(){}
 
-    virtual bool findRelation(std::shared_ptr<ProtocolGraph> protocol) = 0;
+    virtual bool findRelation(std::shared_ptr<ProtocolGraph> protocol,int nargs, ...) throw(std::invalid_argument) = 0;
     virtual int getMappedComponent(const std::string & virtualContainer) = 0;
 };
 #endif // MAPPINGINTERFACE_H
